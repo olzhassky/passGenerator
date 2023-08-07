f2871e77e7b719973ac18265d2200223a8c8c3ce
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-08-07 16:14:20 +0600

    Initial Commit

---
+++ passGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passGen
+//
+//  Created by Olzhas Zhakan on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
